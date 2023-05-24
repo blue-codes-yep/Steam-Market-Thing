618b1287aff58998a15e217b376646aaa0de6c83
blue-codes-yep <linzblue5@gmail.com>
2023-05-24 04:57:34 -0400

    Getting #Items, Starting Prices, Item Name, Game Name

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use scraper::{Html, Selector};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://steamcommunity.com/market/").await?;
+    let body = resp.text().await?;
+
+    let fragment = Html::parse_document(&body);
+    let selector = Selector::parse("a.market_listing_row_link").unwrap();
+
+    for element in fragment.select(&selector) {
+        let text = element.text().collect::<Vec<_>>();
+        let cleaned_data: Vec<String> = text.into_iter()
+            .map(|item| item.replace("\n", "").replace("\t", ""))
+            .filter(|item| !item.is_empty())
+            .collect();
+        println!("{:?}", cleaned_data);
+    }
+
+    Ok(())
 }
