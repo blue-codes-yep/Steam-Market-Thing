383aa1f1c1e610f89b60f2f73c2d1dac8dc737be
blue-codes-yep <linzblue5@gmail.com>
2023-05-24 05:10:18 -0400

    Cleaned up data

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let selector = Selector::parse("a.market_listing_row_link").unwrap();
 
     for element in fragment.select(&selector) {
-        let text = element.text().collect::<Vec<_>>();
+        let text: Vec<_> = element.text().collect();
         let cleaned_data: Vec<String> = text.into_iter()
             .map(|item| item.replace("\n", "").replace("\t", ""))
             .filter(|item| !item.is_empty())
             .collect();
-        println!("{:?}", cleaned_data);
+        if cleaned_data.len() >= 6 {
+            let formatted_data = vec![
+                "Amount of items:".to_string(),
+                cleaned_data[0].clone(),
+                "Starting at:".to_string(),
+                cleaned_data[2].clone(),
+                "Lowest Price:".to_string(),
+                cleaned_data[3].clone(),
+                "Item:".to_string(),
+                cleaned_data[4].clone(),
+                "Game:".to_string(),
+                cleaned_data[5].clone(),
+            ];
+            println!("{:?}", formatted_data);
+        }
     }
 
     Ok(())
